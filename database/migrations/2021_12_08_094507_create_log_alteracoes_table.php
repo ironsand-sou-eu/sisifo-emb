2790559050510037dff5cc1fbdf2bfc29145a800
Shift <shift@laravelshift.com>
2022-02-19 00:13:21 +0000

    Adopt anonymous migrations

--- database/migrations/2021_12_08_094507_create_log_alteracoes_table.php
+++ database/migrations/2021_12_08_094507_create_log_alteracoes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLogAlteracoesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -36,4 +36,4 @@ class CreateLogAlteracoesTable extends Migration
     {
         Schema::dropIfExists('log_alteracoes');
     }
-}
+};
